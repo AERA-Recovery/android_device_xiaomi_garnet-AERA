2bd5230e2ca5e4f0f0e6834af0646d971f003413
Darth9 <reyprimerjefe@gmail.com>
2026-04-20 17:23:09 +0100

    Update the Magisk addon to v30.7

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -62,7 +62,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	# sm84xx-common
 	fetch_sm84xx_common_repo;
 
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v28.1.zip
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v30.7.zip
 	export FOX_VIRTUAL_AB_DEVICE=1
         export FOX_VANILLA_BUILD=1
     	export FOX_ENABLE_APP_MANAGER=1
