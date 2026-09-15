4c5f5b81f7a2c75fdbffc6e5346b146273a459a4
Darth9 <reyprimerjefe@gmail.com>
2026-09-15 23:06:56 +0100

    Include the busybox binary

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -79,6 +79,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_NANO_EDITOR=1
     	export FOX_DELETE_AROMAFM=1
 	export FOX_USE_DATE_BINARY=1
+	export FOX_USE_BUSYBOX_BINARY=1
 
 	# KSU, etc.
 	export FOX_ENABLE_KERNELSU_SUPPORT=1
