568337333df1dcfb77322efa91ed967317d49251
Darth9 <reyprimerjefe@gmail.com>
2026-09-15 22:09:57 +0100

    Avoid encryption issues on Android 17 ROMs

--- fox_garnet.mk
+++ fox_garnet.mk
@@ -46,7 +46,7 @@ OF_UNBIND_SDCARD_F2FS := 1
 OF_WIPE_METADATA_AFTER_DATAFORMAT := 1
 
 # avoid MTP issues after data format
-OF_BIND_MOUNT_SDCARD_ON_FORMAT := 1
+# OF_BIND_MOUNT_SDCARD_ON_FORMAT := 1
 
 # don't spam the console with loop errors
 OF_LOOP_DEVICE_ERRORS_TO_LOG := 1
